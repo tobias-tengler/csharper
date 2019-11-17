f4c2ff7302b4843791fc59d517bb0c007104f1bd
Tobias Tengler <45513122+tobias-tengler@users.noreply.github.com>
2019-11-17 18:23:45 +0100

    Add newFile command and calculate filepath

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,49 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import * as fs from "fs";
+import * as path from "path";
 
-export function activate(context: vscode.ExtensionContext) { }
+export function activate(context: vscode.ExtensionContext) {
+  context.subscriptions.push(
+    vscode.commands.registerCommand("csharper.newFile", newFile)
+  );
+}
 
-export function deactivate() { }
+export function deactivate() {}
+
+function newFile(args: any) {
+  vscode.window
+    .showInputBox({
+      ignoreFocusOut: true,
+      prompt: "Please enter a filename"
+    })
+    .then(input => {
+      if (!input) {
+        return;
+      }
+
+      const filepath = getFilePath(args.fsPath, getFilename(input));
+
+      if (fs.existsSync(filepath)) {
+        vscode.window.showErrorMessage("File already exists");
+        return;
+      }
+
+      console.log(filepath);
+    });
+}
+
+function getFilename(filename: string) {
+  if (path.extname(filename) !== ".cs") {
+    if (filename.endsWith(".")) {
+      return filename + "cs";
+    }
+
+    return filename + ".cs";
+  }
+
+  return filename;
+}
+
+function getFilePath(dir: string, filename: string) {
+  return dir + path.sep + filename;
+}
